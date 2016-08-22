56f6b1a4c06f9495d8405a69fd9e87f02b1feb7a
Jost <jost.pristavec@gmail.com>
2016-08-22 21:13:18 +0200

    RomanNumbersConverter - converting from arabic to roman implementation

---
+++ RomanNumberConverter/RomanNumberConverter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumberConverter {
+
+    public class RomanNumberConverter {
+
+        private static string[] RomanChars = new string[] { "I", "V", "X", "L", "C", "D", "M" };
+
+        public static string ConvertToRoman(string number) {
+            try {
+                int intNo = int.Parse(number);
+
+                if(intNo < 1) {
+                    throw new Exception("Negative integer.");
+                }
+            } catch (Exception) {
+                throw new ArithmeticException("Number: " + number + " is not valid number. Valid numbers are only positive integers.");
+            }
+
+            string enice = "", desetice = "", stotice = "", tisocice = "", tmpInteger;
+            int numberLength = number.Length;
+
+            // pos:0 = 0-9
+            tmpInteger = number.Substring(numberLength - 1);
+            enice = ConvertIntToRoman(int.Parse(tmpInteger), 0);
+
+            // pos:1 = 10-99
+            if (numberLength > 1) {
+                tmpInteger = number.Substring(numberLength - 2, 1);
+                desetice = ConvertIntToRoman(int.Parse(tmpInteger), 1);
+            }
+
+            // pos:2 = 100-999
+            if (numberLength > 2) {
+                tmpInteger = number.Substring(numberLength - 3, 1);
+                stotice = ConvertIntToRoman(int.Parse(tmpInteger), 2);
+            }
+
+            // pos:3 = > 1000
+            if (numberLength > 3) {
+                tmpInteger = number.Substring(numberLength - 4, 1);
+                tisocice = ConvertIntToRoman(int.Parse(tmpInteger), 3);
+            }
+
+            return tisocice + stotice + desetice + enice;
+        }
+
+        private static string ConvertIntToRoman(int number, int indexPosition) {
+            StringBuilder sb = new StringBuilder();
+
+            // tisocice
+            if (indexPosition > 2) {
+                for (int i = 0; i < number; i++) {
+                    sb.Append(RomanChars[6]);
+                }
+            } else {
+                int k = 0;  // offset in RomanChars. k => 1, k+1 => 5, k+2 => 10
+                if (indexPosition == 0) {   // enice
+                    k = 0;
+                } else if (indexPosition == 1) {    // desetice
+                    k = 2;
+                } else {
+                    k = 4;  // stotice
+                }
+
+                if (number == 4) {
+                    sb.Append(RomanChars[k]);
+                    sb.Append(RomanChars[k + 1]);
+                } else if (number == 5) {
+                    sb.Append(RomanChars[k + 1]);
+                } else if (number == 9) {
+                    sb.Append(RomanChars[k]);
+                    sb.Append(RomanChars[k + 2]);
+                } else if (number < 4) {
+                    for (int i = 0; i < number; i++) {
+                        sb.Append(RomanChars[k]);
+                    }
+                } else {
+                    sb.Append(RomanChars[k + 1]);
+                    for (int i = 0; i < number; i++) {
+                        sb.Append(RomanChars[k]);
+                    }
+                }
+
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ RomanNumberConverterTest/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumberConverterTest {
+    [TestClass]
+    public class Test {
+        [TestMethod]
+        public void TestConvertToRoman() {
+
+            // osnovne
+            Assert.AreEqual("I", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("1"));
+            Assert.AreEqual("V", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("5"));
+            Assert.AreEqual("X", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("10"));
+            Assert.AreEqual("L", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("50"));
+            Assert.AreEqual("C", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("100"));
+            Assert.AreEqual("D", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("500"));
+            Assert.AreEqual("M", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("1000"));
+
+            // posebnosti
+            Assert.AreEqual("IV", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("4"));
+            Assert.AreEqual("XL", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("40"));
+            Assert.AreEqual("CD", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("400"));
+
+            Assert.AreEqual("IX", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("9"));
+            Assert.AreEqual("XC", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("90"));
+            Assert.AreEqual("CM", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("900"));
+
+            // random
+            Assert.AreEqual("MCMXLV", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("1945"));
+            Assert.AreEqual("MM", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("2000"));
+            Assert.AreEqual("MCM", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("1900"));
+            Assert.AreEqual("MCMXIX", RomanNumberConverter.RomanNumberConverter.ConvertToRoman("1919"));
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException), "Number: is not valid number.Valid numbers are only positive integers.")]
+        public void TestConvertToRomanException() {
+            RomanNumberConverter.RomanNumberConverter.ConvertToRoman("-1");
+            RomanNumberConverter.RomanNumberConverter.ConvertToRoman("a");
+            RomanNumberConverter.RomanNumberConverter.ConvertToRoman("");
+            RomanNumberConverter.RomanNumberConverter.ConvertToRoman("22.3");
+        }
+    }
+}
